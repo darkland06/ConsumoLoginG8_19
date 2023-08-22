17438d21dd99af9466f746bfb0ccc8fe3735eb2a
darkland06 <darlan.reyes@unah.hn>
2023-08-22 13:17:51 -0600

    Creacion de la lista de usuarios

--- JS/Usuario.js
+++ JS/Usuario.js
@@ -1,4 +1,38 @@
 var UrlApiInsert = 'http://localhost:5008/usuario/insertar/:codigo_usuario';
+var UrlApiGetAll = 'http://localhost:5008/usuario/getall';
+$(document).ready(function(){
+     CargarUsuarios();
+ });
+ function CargarUsuarios(){
+     $.ajax({
+         url: UrlApiGetAll,
+         type:'GET',
+         datatype:'JSON',
+         success:function(response){
+             var MiItems = response;
+             var Valores ='';
+             for(i=0; i < MiItems.length; i++){
+                 Valores +=
+                 '<tr>'+
+                 '<td>'+ MiItems[i].codigo_usuario +'</td>'+
+                 '<td>'+ MiItems[i].nombre +'</td>'+
+                 '<td>'+ MiItems[i].apellido +'</td>'+
+                 '<td>'+ MiItems[i].clave +'</td>'+
+                 '<td>'+ MiItems[i].email +'</td>'+
+                 '<td>'+ MiItems[i].estado +'</td>' +
+                 '<td>'+ MiItems[i].ultimo_ingreso +'</td>' +
+                 '<td>'+ MiItems[i].clave_expira +'</td>' +
+                 '<td>'+ MiItems[i].dias_caducidad +'</td>' +
+                 '<td>'+ MiItems[i].rol +'</td>' +
+                 '<td>'+ MiItems[i].intentos +'</td>' +
+                 '<td>'+ MiItems[i].fecha_registro +'</td>' +    
+                 '<td>'+
+                 '</tr>';
+                 $('#DataUs').html(Valores);
+             }
+         }
+     });
+ }
 
 function AgregarUsuario(){
     var datosUsuario = {
